ea898a1eb72d2dad4ad83a1d39f0791fcbcbd5b7
Ketan-Goyal <ketan.ug20@nsut.ac.in>
2022-06-18 00:46:14 +0530

    pause

--- lib/data/controllers/popular_product_controller.dart
+++ lib/data/controllers/popular_product_controller.dart
@@ -1,16 +1,20 @@
 import 'package:get/get.dart';
 import 'package:go_foodz/data/repository/popular_product_repo.dart';
 
+import '../../models/products_model.dart';
+
 class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
   List<dynamic> _popularProductList = [];
   List<dynamic> get popularProductList => _popularProductList;
+
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
+      print("got products");
       _popularProductList = [];
-      // _popularProductList.addAll();
+      _popularProductList.addAll(Product.fromJson(response.body).products);
       update();
     } else {}
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:go_foodz/data/controllers/popular_product_controller.dart';
 import 'package:go_foodz/helper/dependencies.dart' as dep;
 import 'package:go_foodz/screens/food/Recommended_food_details.dart';
 
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.find<PopularProductController>().getPopularProductList();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
